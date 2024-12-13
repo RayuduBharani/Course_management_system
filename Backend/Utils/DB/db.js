b93d0996d9758bcaf081dc122dfcfeaa35465151
Rayudu Bharani <rayudubharani7288@gmail.com>
2024-12-13 23:43:11 +0530

    mongoUrl added

--- Backend/Utils/DB/db.js
+++ Backend/Utils/DB/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // mongodb://localhost:27017/CMS
 const db = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/CMS");
+        await mongoose.connect("mongodb+srv://Bharani_Rayudu:Bharani@cluster0.bsh0jfz.mongodb.net/CMS?retryWrites=true&w=majority&appName=Cluster0");
         console.log('MongoDB connected');
     } 
     catch (error) {
