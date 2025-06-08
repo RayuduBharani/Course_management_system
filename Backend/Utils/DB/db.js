892f3e909bcc77f0fa342e71785f18998de87210
Rayudu Bharani <rayudubharani7288@gmail.com>
2025-06-08 21:21:36 +0530

    Completed the project - 1

--- Backend/Utils/DB/db.js
+++ Backend/Utils/DB/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 // 'mongodb+srv://Bharani_Rayudu:Bharani@cluster0.bsh0jfz.mongodb.net/CMS?retryWrites=true&w=majority&appName=Cluster0'
 // mongodb://localhost:27017/CMS
-const db = async () => {
+const db = async () => {    
     try {
-        await mongoose.connect("mongodb://localhost:27017/CMS");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } 
     catch (error) {
